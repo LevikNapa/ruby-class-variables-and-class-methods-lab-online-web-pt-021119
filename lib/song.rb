1c69b999a4ea31d792ca010be64d88f795f446f4
Levik Naparstek <levik010@gmail.com>
2019-03-11 21:34:42 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song 
+  attr_accessor :name, :artist, :genre
+  
+    @@count = 0
+    @@genres = []
+    @@artists = []
+    def self.count
+      @@count = count
+    end
+    def self.genre
+       @@genres = genre
+    end
+    def self.artists
+      @@artists = artist
+    end
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist 
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+  
+end 
